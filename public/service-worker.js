0b4fd9d353d7f5d46fceb42a2c7a4ad95cc888e8
riraq <riraq@mail.usf.edu>
2021-05-06 17:36:23 -0500

    updated service worker and manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,9 @@ const FILES_TO_CACHE = [
   '/index.js',
   '/db.js',
   '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  "/manifest.webmanifest",
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
